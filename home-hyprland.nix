8fc97e53d7cd6ab3b49a78223beb80746c10d857
rickyrnt <rick.yarnot.255@gmail.com>
2025-04-05 01:31:29 -0400

    Don't worry about kbd bcklght

--- home-hyprland.nix
+++ home-hyprland.nix
@@ -176,12 +176,12 @@ in
 					on-timeout = "${pkgs.brightnessctl}/bin/brightnessctl -s set 10";         # set monitor backlight to minimum, avoid 0 on OLED monitor.
 					on-resume = "${pkgs.brightnessctl}/bin/brightnessctl -r";                 # monitor backlight restore.
 				}
-				{
+				# {
 				# turn off keyboard backlight, comment out this section if you dont have a keyboard backlight.
-					timeout = 150;                                          # 2.5min.
-					on-timeout = "${pkgs.brightnessctl}/bin/brightnessctl -sd rgb:kbd_backlight set 0"; # turn off keyboard backlight.
-					on-resume = "${pkgs.brightnessctl}/bin/brightnessctl -rd rgb:kbd_backlight";        # turn on keyboard backlight.
-				}
+					# timeout = 150;                                          # 2.5min.
+					# on-timeout = "${pkgs.brightnessctl}/bin/brightnessctl -sd rgb:kbd_backlight set 0"; # turn off keyboard backlight.
+					# on-resume = "${pkgs.brightnessctl}/bin/brightnessctl -rd rgb:kbd_backlight";        # turn on keyboard backlight.
+				# }
 				{
 					timeout = 300;                                 # 5min
 					on-timeout = "loginctl lock-session";            # lock screen when timeout has passed
