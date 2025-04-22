c62050735ab9e54061bb195e945bdf7ef0daea03
rickyrnt <rick.yarnot.255@gmail.com>
2025-04-22 19:58:29 -0400

    Add in a formatter

--- flake.nix
+++ flake.nix
@@ -2,29 +2,45 @@
   description = "The rickyrnt personal system";
 
   inputs = {
-    nixpkgs.url = github:nixos/nixpkgs/nixos-24.11;
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
 
     home-manager = {
-      url = github:nix-community/home-manager/0b491b460f52e87e23eb17bbf59c6ae64b7664c1;
+      url = "github:nix-community/home-manager/0b491b460f52e87e23eb17bbf59c6ae64b7664c1";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     hyprland = {
-      url = github:hyprwm/hyprland;
+      url = "github:hyprwm/hyprland";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    
+
     rose-pine-hyprcursor = {
-      url = github:ndom91/rose-pine-hyprcursor;
+      url = "github:ndom91/rose-pine-hyprcursor";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.hyprlang.follows = "hyprland/hyprlang";
     };
-  };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    nixosConfigurations.M04RYS8 = nixpkgs.lib.nixosSystem {
-      specialArgs = { inherit inputs; };
-      modules = [ ./configuration.nix ];
+    pre-commit-hooks = {
+      url = "github:cachix/git-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    {
+      checks.x86_64-linux.pre-commit-check = inputs.pre-commit-hooks.lib.x86_64-linux.run {
+        src = ./.;
+        hooks = {
+          nixfmt-rfc-style.enable = true;
+        };
+      };
+
+      nixosConfigurations.M04RYS8 = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [ ./configuration.nix ];
+      };
+
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
+    };
 }
