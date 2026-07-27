0b871f9e1ce6317f32c573363db24841ab74a5c1
rickyrnt <rick.yarnot.255@gmail.com>
2026-07-27 15:14:08 -0700

    get audio working on chromebook

--- machines/A0LA/configuration.nix
+++ machines/A0LA/configuration.nix
@@ -89,9 +89,37 @@
   };
   services.xserver.videoDrivers = [ "modesetting" ];
   
-  environment.systemPackages = with pkgs; [
-    libinput-gestures
-  ];
+  environment = let 
+    alsa-ucm-conf-chromebook = with pkgs; alsa-ucm-conf.overrideAttrs {
+      wttsrc = fetchFromGitHub {
+        owner = "WeirdTreeThing";
+        repo = "chromebook-ucm-conf";
+        rev = "b6ce2a7";
+        hash = "sha256-QRUKHd3RQmg1tnZU8KCW0AmDtfw/daOJ/H3XU5qWTCc=";
+      };
+      postInstall = ''
+        echo "v0.4.1" > $out/chromebook.patched
+
+        # Asus Chromebook CX1400 (GALTIC)
+        #   ❯ aplay -l
+        #   card 0: sofrt5682 [sof-rt5682], device 0: Headphones (*) []
+        #   ....
+        cp -R $wttsrc/sof-rt5682 $out/share/alsa/ucm2/conf.d
+        cp -R $wttsrc/common/* $out/share/alsa/ucm2/common
+        cp -R $wttsrc/codecs/* $out/share/alsa/ucm2/codecs
+        cp -R $wttsrc/platforms/* $out/share/alsa/ucm2/platforms
+      '';
+    };
+  in { 
+    systemPackages = with pkgs; [
+      libinput-gestures
+      alsa-ucm-conf
+      sof-firmware
+    ];
+    sessionVariables = {
+      ALSA_CONFIG_UCM2 = "${alsa-ucm-conf-chromebook}/share/alsa/ucm2";
+    };
+  };
   
   programs.iio-hyprland.enable = true;
 
