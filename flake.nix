f2e109114e6c0ded93fd679dbfc1f4a2603c3b30
rickyrnt <rick.yarnot.255@gmail.com>
2026-08-06 19:36:38 -0700

    Choose stable hyprgrass commit

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
     };
 
     hyprgrass = {
-     url = "github:horriblename/hyprgrass";
+     url = "github:horriblename/hyprgrass/v0.8.2";
      inputs.hyprland.follows = "hyprland"; # IMPORTANT
     };
     
